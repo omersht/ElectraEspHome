145803c247b4e72a7afc3fc82353b973f30c8ba2
omer <90841289+omersht@users.noreply.github.com>
2024-10-13 17:31:36 +0300

    Update electra.h

--- components/electra/electra.h
+++ components/electra/electra.h
@@ -72,7 +72,7 @@ class ElectraClimate : public climate_ir::ClimateIR {
  protected:
 
   ElectraCode decode_electra(remote_base::RemoteReceiveData &data);
-  #define ELECTRA_TIME_UNIT 975
+  #define ELECTRA_TIME_UNIT 1000
   #define ELECTRA_NUM_BITS 34
   bool supportsOff;
   /// declartion of active mode value
