3df5d628ca73a38300028c4b34517b08cac439fe
omer <90841289+omersht@users.noreply.github.com>
2024-10-14 18:27:20 +0300

    Update electra.h

--- components/electra/electra.h
+++ components/electra/electra.h
@@ -72,9 +72,6 @@ class ElectraClimate : public climate_ir::ClimateIR {
  protected:
 
   /// declartion of variables
-  #define ELECTRA_DECODE_TRIPLE_MARK_TIME_UNIT 3000
-  #define ELECTRA_DECODE_DOUBLE_MARK_TIME_UNIT 1825
-  #define ELECTRA_DECODE_DOUBLE_SPACE_TIME_UNIT 2100
   #define ELECTRA_TIME_UNIT 1000
   #define ELECTRA_NUM_BITS 34
   bool supportsOff;
@@ -90,8 +87,6 @@ class ElectraClimate : public climate_ir::ClimateIR {
   /// decodes the incoming signal
   ElectraCode decode_electra(remote_base::RemoteReceiveData data);
   ElectraCode analyze_electra(remote_base::RemoteReceiveData &data);
-  bool check_electra_mark(remote_base::RemoteReceiveData &data);
-  bool check_electra_space(remote_base::RemoteReceiveData &data);
 };
 
 
